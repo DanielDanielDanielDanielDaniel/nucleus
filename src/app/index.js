3ef7acfe1895936a3b71b5c93ba27028f32e50d9
Sara Moreno <sara@redradix.com>
2018-03-22 18:29:09 +0100

    feat(app): integrate all sections

--- src/app/index.js
+++ src/app/index.js
@@ -2,11 +2,22 @@ import React, { Component } from 'react'
 import sectionWatcher from 'lib/section-watcher'
 import Layout from 'app/layout'
 import SiteMenu from 'app/site-menu'
+
 import Hero from 'app/hero'
+import UsecaseSteak from 'app/usecase-steak'
+import UsecaseWatch from 'app/usecase-watch'
+import UsecaseJet from 'app/usecase-jet'
+import DesignSpecification from 'app/design-specifications'
+import HowCanBeUsed from 'app/how-can-be-used'
+import Features from 'app/features'
+import Contact from 'app/contact'
+
 import ScrollSpy from 'scroll-spy'
-import ContentBlock from 'app/test-content-block'
-import FadeIn from 'scroll-effects/fade-in'
-import Parallax from 'scroll-effects/parallax'
+// import ContentBlock from 'app/test-content-block'
+// import FadeIn from 'scroll-effects/fade-in'
+// import Parallax from 'scroll-effects/parallax'
+
+import 'normalize.css'
 import styles from './app.module.css'
 
 class App extends Component {
@@ -17,25 +28,13 @@ class App extends Component {
         <Layout>
           <SiteMenu/>
           <ScrollSpy idprop='sectionid' onScroll={sectionWatcher.update}>
-            <div>
-              <h1>Something</h1>
-              <Parallax speed={200}>
-                <h1>Something else</h1>
-              </Parallax>
-            </div>
-            {/*
-            <FadeIn sectionid='block-1'>
-              <ContentBlock />
-            </FadeIn>
-            <FadeIn sectionid='block-2'>
-              <ContentBlock/>
-            </FadeIn>
-            <FadeIn sectionid='block-3'>
-              <ContentBlock />
-            </FadeIn>
-            <ContentBlock sectionid='block-4' />
-            <ContentBlock sectionid='block-5' />
-            */}
+            <UsecaseSteak sectionid='usecases' />
+            <UsecaseWatch sectionid='usecases' />
+            <UsecaseJet sectionid='usecases' />
+            <DesignSpecification sectionid='design-spec' />
+            <HowCanBeUsed sectionid='how-to-use'/>
+            <Features sectionid='features' />
+            <Contact sectionid='contact' />
           </ScrollSpy>
         </Layout>
       </div>
