b12b326c9deb5d7b87c352166a86845fdb6e3752
Sara Moreno <sara@redradix.com>
2018-03-22 18:27:05 +0100

    feat(hero): big initial banner

--- src/app/hero/index.js
+++ src/app/hero/index.js
@@ -1,13 +1,16 @@
-import React, { Component } from 'react'
+import React from 'react'
 import styles from './hero.module.css'
 
-class Hero extends Component {
-  render() {
-    return (
-      <div className={styles.hero}>
-      </div>
-    )
-  }
-}
+const Hero = ()=> (
+  <div className={styles.hero}>
+    <div>
+      <h1 className={styles.title}>Nucleus Iconset</h1>
+      <p className={styles.subtitle}>
+        An identity creating tool with 400 premium icons for <br/>
+        precision, tech and professionalism
+      </p>
+    </div>
+  </div>
+)
 
 export default Hero
