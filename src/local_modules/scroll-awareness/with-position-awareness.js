08881052990d2e25c662e61b7e2ee9c8660853d7
Elias A. G.-C <eliasagc@gmail.com>
2018-03-30 20:36:12 +0200

    fix(parallax): tweaking fo the initial calculation

--- src/local_modules/scroll-awareness/with-position-awareness.js
+++ src/local_modules/scroll-awareness/with-position-awareness.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-import { vmap, vcap } from './lib/utils'
+import { vmap, vcap, nextTick } from './lib/utils'
 
 const SAFE_MARGIN = 100
 const ABOVE = 'above'
@@ -16,6 +16,11 @@ export class PositionInjector extends Component {
   }
   componentDidMount() {
     window.addEventListener('scroll', this.handleScroll)
+    this.calculateRect(true)
+    this.handleScroll()
+    // quick-and-dirty way to recalculate the position
+    // a better way would be to keep a CONFIDENCE COEFICIENT
+    setTimeout(() => this.hardReset(), 100)
   }
   componentWillUnmount() {
     window.removeEventListener('scroll', this.handleScroll)
@@ -26,14 +31,30 @@ export class PositionInjector extends Component {
     }
     return this.node
   }
+  hardReset() {
+    // disable visibility (should disable any fx, specially parallax)
+    this.setHidden()
+    nextTick(() => {
+      // next tick, after render
+      this.calculateRect(true)
+      // reposition
+      this.handleScroll()
+    })
+  }
   calculateRect(forceUpdate = false) {
     const node = this.findDOMNode()
     if (!node) return { height: 0, top: 0 }
     if (forceUpdate || !this.top) {
+      // update the position (can't do it every tick because parallax)
       // cache all this to avoid touching the DOM on subsequent events
       const rect = node.getBoundingClientRect()
       this.top = rect.top + window.scrollY
       this.height = rect.height
+    } else if (this.height === 0) {
+      // update only the height
+      // (can do this freely because does not interfere with parallax)
+      const { height } = node.getBoundingClientRect()
+      this.height = height
     }
     return { top: this.top, height: this.height }
   }
