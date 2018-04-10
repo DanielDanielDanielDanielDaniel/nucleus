1f240f018ac705e830a2a1a188aaed1761e117e7
Elias A. G.-C <eliasagc@gmail.com>
2018-04-11 01:48:08 +0200

    fix(wrapper+grid): better scaling to big screens + better icon grid

--- src/components/composition/icon-category/index.js
+++ src/components/composition/icon-category/index.js
@@ -1,19 +1,22 @@
 import React from 'react'
-import IconGalleryBox from 'components/presentation/icon-gallery-box'
+// import IconGalleryBox from 'components/presentation/icon-gallery-box'
+import IconBox from 'components/presentation/icon-box'
 import VisibilityOpacity from 'scroll-effects/visibility-opacity'
 import styles from './icon-category.module.css'
 
 const path = filename => `/icons/${filename}`
 
 const IconCategory = ({ title, icons, sublabel = '5.3° / 6.2°' }) => (
-  <VisibilityOpacity base={0.1} variance={0.9} easing='easeInCubic'>
-    <div className={styles.container}>
-      <h4 className={styles.title}>{title}</h4>
-      <div className={styles.iconGrid}>
-        {icons.map(icon => <IconGalleryBox key={icon.filename} src={path(icon.filename)} label={icon.name} sublabel={sublabel}/>)}
-      </div>
+  <div className={styles.container}>
+    <h4 className={styles.title}>{title}</h4>
+    <div className={styles.iconGrid}>
+      {icons.map(icon => (
+        <VisibilityOpacity key={icon.filename} base={0.1} variance={0.9} easing='easeInQuad'>
+          <IconBox src={path(icon.filename)} label={icon.name}/>
+        </VisibilityOpacity>
+      ))}
     </div>
-  </VisibilityOpacity>
+  </div>
 )
 
 export default IconCategory
