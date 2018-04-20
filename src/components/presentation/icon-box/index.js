91b3e149eed29521c4d7ad504cf5a16ab11e2d21
Elias A. G.-C <eliasagc@gmail.com>
2018-04-20 19:07:19 +0200

    fix(homepage): js error was interrupting the page load on safari

--- src/components/presentation/icon-box/index.js
+++ src/components/presentation/icon-box/index.js
@@ -6,9 +6,8 @@ const IconBox = ({ src, label, style }) => (
   <div className={cn(styles.boxcontainer)} style={style}>
   <div className={cn(styles.box, 'icon-box')} style={style}>
     <div className={styles.iconContainer}>
-      <pre>{JSON.stringify()}</pre>
       <img className={styles.icon} src={src} alt={label}/>
-    </div>  	
+    </div>
   </div>
    	<span className={styles.label}>{label}</span>
   </div>
