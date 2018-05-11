5283d51cf3993ee65cc1d9d652a82a49ebec63fa
Daniel <daniel@hackerbay.com>
2018-05-11 12:28:53 +0200

    css award badge

--- src/app/header/index.js
+++ src/app/header/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import styles from './header.module.css'
+import badge from './assets/cssda-nominee-purple-vote.png'
+
 
 const Header = ({ onOpenMenu }) => (
   <div className={styles.header}>
@@ -8,6 +10,12 @@ const Header = ({ onOpenMenu }) => (
           onTouchStart={onOpenMenu}/>
     <div className={styles.buttonBox}>
       <a className={styles.button} href='https://ui8.net/products/nucleus-icon-set-v3?rel=doge'>Buy Set</a>
+    </div>
+    <div className={styles.awardBadge}>
+    <a href='https://www.cssdesignawards.com/sites/nucleus-iconset/32906/'>
+    	<img src={badge} alt=''/>
+    </a>
+      
     </div>
   </div>
 )
